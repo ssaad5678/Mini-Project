1819cdc464cf05ff30012670f972cba18cf1d402
ssaad5678 <84627426+ssaad5678@users.noreply.github.com>
2023-05-05 02:45:46 +0530

    Create the-last-test.py

---
+++ Online-Cab-Trip-Analysis/the-last-test.py
@@ -0,0 +1,172 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+
+# Reading data
+df = pd.read_csv("Uber Trip.csv", encoding='latin1')
+df.columns = df.columns.str.replace("*","")
+
+# Missing Data
+st.header('Missing Data')
+st.write('Heatmap of missing data')
+fig=plt.figure(figsize=(15,10))
+sns.heatmap(df.isnull(), cmap='magma', yticklabels=False)
+st.pyplot(fig)
+
+st.write('Bar chart of missing data')
+msno.bar(df)
+st.pyplot()
+
+# Handling missing data
+df.drop(index=1155, axis=0, inplace=True)
+df['PURPOSE'].fillna(method='ffill', inplace=True)
+
+# Data cleaning
+df['START_DATE'] = pd.to_datetime(df['START_DATE'], errors='coerce')
+df['END_DATE'] = pd.to_datetime(df['END_DATE'], errors='coerce')
+
+# Categorical data
+st.header('Categorical Data')
+category = pd.crosstab(index=df['CATEGORY'], columns='count of travel as per category')
+st.write(category)
+category.plot(kind='bar', color='r')
+plt.legend()
+st.pyplot()
+
+st.write('Start points with more than 10 trips')
+start_point = df.START.value_counts()
+st.write(start_point[start_point>10])
+start_point[start_point>10].plot(kind='pie', shadow=True)
+st.pyplot()
+
+st.write('Start points with 10 or fewer trips')
+st.write(start_point[start_point<=10])
+
+st.write('Stop points with more than 10 trips')
+stop_point = df.STOP.value_counts()
+st.write(stop_point[stop_point>10])
+
+st.write('Stop points with 10 or fewer trips')
+st.write(stop_point[stop_point<=10])
+
+st.write('Miles with more than 10 trips')
+miles = df.MILES.value_counts()
+st.write(miles[miles>10])
+miles[miles>10].plot(kind='bar')
+st.pyplot()
+
+st.write('Miles with 10 or fewer trips')
+st.write(miles[miles<=10])
+data = {'MILES': [840, 31],
+        'Round_TRIP': [False, True]}
+
+dfe = pd.DataFrame(data)
+
+st.dataframe(dfe)
+
+miles = pd.crosstab(index=df['MILES']>10, columns='count of miles')
+miles.plot(kind='bar', color='r')
+st.pyplot()
+
+st.write('Trips per Purpose')
+st.write(df.PURPOSE.value_counts())
+
+# Number of Trips per Purpose
+purpose_counts = df.groupby('PURPOSE')['PURPOSE'].count().sort_values(ascending=False)
+plt.figure(figsize=(15,6))
+purpose_counts.plot(kind='bar', color='blue')
+plt.xlabel('Purpose')
+plt.ylabel('Count')
+plt.title('Number of Trips per Purpose')
+st.pyplot()
+
+# Trip duration
+df['minutes'] = df.END_DATE - df.START_DATE
+df['minutes'] = df['minutes'].dt.total_seconds()/60
+
+st.write('Trip Duration')
+st.write(pd.DataFrame({'Mean': df.groupby(['PURPOSE'])['MILES'].mean().round(1),
+                       'Min': df.groupby(['PURPOSE'])['MILES'].min(),
+                       'Max': df.groupby(['PURPOSE'])['MILES'].max()}
+                      ).reset_index())
+
+ax=plt.figure(figsize=(16,7))
+plt.subplot(1,2,1)
+sns.boxplot(data=df,x=df.PURPOSE, y=df.MILES)
+plt.xticks(rotation=45)
+plt.subplot(1,2,2)
+sns.boxplot(data=df,x=df.PURPOSE,y=df.minutes)
+plt.xticks(rotation=45)
+st.pyplot(ax)
+ab=plt.figure(figsize=(16,7))
+plt.subplot(1,2,1)
+sns.boxplot(data=df,x=df.PURPOSE,y=df.MILES,showfliers=False)
+plt.xticks(rotation=45)
+plt.subplot(1,2,2)
+sns.boxplot(data=df,x=df.PURPOSE,y=df.minutes,showfliers=False)
+plt.xticks(rotation=45)
+st.pyplot(ab)
+
+st=plt.figure(figsize=(8,5))
+# Create a new column indicating whether a trip is round or not
+df['Round_TRIP'] = df.apply(lambda x: 'yes' if x['START']==x['STOP'] else 'no', axis=1)
+# Count the number of trips for each category of Round_TRIP and create a bar plot
+round_trip_counts = df['Round_TRIP'].value_counts().sort_values(ascending=False)
+round_trip_counts.plot(kind='bar', color='blue')
+# Set the axis labels and title
+plt.xlabel('Round Trip')
+plt.ylabel('Count')
+plt.title('Number of Round Trips')
+st.pyplot(st)
+df['month']= pd.DatetimeIndex(df['START_DATE']).month
+dic = {1:'jan',2:'feb',3:'mar',4:'apr',5:'may',6:'jun',7:'jul',8:'aug',9:'sept',10:'oct',11:'nov',12:'dec'}
+df['month'] =df['month'].map(dic)
+st.write(df)
+plt.figure(figsize=(12,7))
+sns.countplot(x='month', data=df, order=df['month'].value_counts().index, palette='deep')
+plt.xlabel('Month')
+plt.ylabel('Count')
+plt.title('Number of Trips per Month')
+st.pyplot()
+plt.figure(figsize=(12,7))
+round_trip_counts = df.groupby(['Round_TRIP', 'month'])['Round_TRIP'].count().unstack()
+round_trip_counts.plot(kind='bar', stacked=True)
+plt.xlabel('Round Trip')
+plt.ylabel('Count')
+plt.title('Number of Trips per Round Trip and Month')
+st.pyplot()
+plt.figure(figsize=(16,7))
+plt.subplot(1,2,1)
+sns.lineplot(data=df,x=df.minutes,y=df.MILES)
+plt.title('Line Plot')
+plt.subplot(1,2,2)
+sns.scatterplot(data=df,x=df.minutes,y=df.MILES)
+plt.title('Scatter Plot')
+plt.figure(figsize=(9,5))
+sns.countplot(data=df,x='PURPOSE',hue='CATEGORY',dodge=False)
+plt.xticks(rotation=45)
+st.pyplot()
+#Display the bar plots of start and stop locations
+st.subheader('Bar Plots of Start and Stop Locations')
+plt.figure(figsize=(15,4))
+pd.Series(df['START']).value_counts()[:25].plot(kind='bar')
+plt.title('Car rides start location frequency')
+plt.xticks(rotation=45)
+st.pyplot()
+
+plt.figure(figsize=(15,4))
+pd.Series(df['STOP']).value_counts()[:25].plot(kind='bar')
+plt.title('cab rides stop location frequency')
+plt.xticks(rotation=45)
+st.pyplot()
+st.subheader('Conclusions')
+st.write("1.Business cabs were not only used more in volumne but also have travelled more distance.")
+st.write("2.Round trips were more in decemnber")
+st.write("3.december can prove to be the best month for earning profit by raising fare as demand is more")
+st.write("4.seasonal pattern is there")
+st.write("5.Cab traffic was high in just 5 cities comparitevely")
+("6.most of the cab rides are within a distance of 35 miles taking about 30 minutes")
+("7.For Airport cabs are taking more time than usual.")
